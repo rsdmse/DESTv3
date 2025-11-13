e5758257bf81901ff4f45702e60b55716da44f1a
Alan Bergland <aob2x@virginia.edu>
2025-11-13 12:49:11 -0500

    options

--- mappingPipeline/scripts/fq_to_sync_pipeline.v3.0.0.oldOptions.sh
+++ mappingPipeline/scripts/fq_to_sync_pipeline.v3.0.0.oldOptions.sh
@@ -54,6 +54,7 @@ check_exit_status () {
       -h|--help)
       echo "Usage:"
       echo "  singularity run [options] <image> -h          Display this help message."
+      echo "  Pair-end reads mode add --sequencing = 'pe'; Single-end mode add --sequencing = 'se'"
       echo "	Pair-end reads mode requires:"
       echo "  singularity run [options] <image> <fastq_file_1_path> <fastq_file_2_path> <sample_name> <output_dir> <num_cores>"
       echo "	Single-end reads mode requires:"
@@ -61,98 +62,78 @@ check_exit_status () {
       exit 0
       shift # past argument
       ;;
-      -fq1|--fastq_1)
-      read1="$1"
-      shift # past argument
-      shift # past value
-      ;;
-      -fq2|--fastq_2)
-      read2="$1"
-      shift # past argument
-      shift # past value
-      ;;
-      -sample|--sample_name)
-      sample=$"$1"
-      shift # past argument
-      shift # past value
-      ;;
-      -output|--output_directory)
-      output=$"$1"
-      shift # past argument
-      shift # past value
-      ;;
       -bq|--base-quality-threshold)
-      base_quality_threshold="$1"
+      base_quality_threshold="$2"
       shift # past argument
       shift # past value
       ;;
       -ill|--illumina-quality-coding)
-      illumina_quality_coding="$1"
+      illumina_quality_coding="$2"
       shift # past argument
       shift # past value
       ;;
       -mindel|--min-indel)
-      minIndel="$1"
+      minIndel="$2"
       shift # past argument
       shift # past value
       ;;
       -tr|--threads)
-      threads="$1"
+      threads="$2"
       shift # past argument
       shift # past value
       ;;
       -x|--max-cov)
-      max_cov="$1"
+      max_cov="$2"
       shift # past argument
       shift # past value
       ;;
       -n|--min-cov)
-      min_cov="$1"
+      min_cov="$2"
       shift # past argument
       shift # past value
       ;;
       -t|--theta)
-      theta=$1
+      theta=$2
       shift # past argument
       shift # past value
       ;;
       -D)
-      D=$1
+      D=$2
       shift # past argument
       shift # past value
       ;;
       -p|--priortype)
-      theta=$1
+      theta=$2
       shift # past argument
       shift # past value
       ;;
       -f|--fold)
-      fold=$1
+      fold=$2
       shift # past argument
       shift # past value
       ;;
       -ms|--maxsnape)
-      maxsnape=$1
+      maxsnape=$2
       shift # past argument
       shift # past value
       ;;
       -nf|--num-flies)
-      nflies=$1
+      nflies=$2
       shift # past argument
       shift # past value
       ;;
       -ref|--reference_genome)
-      ref=$1
+      ref=$2
       shift # past argument
       shift # past value
       ;;
       -focalFile|--focal_file)
-      focalFile=$1
+      focalFile=$2
       shift # past argument
       shift # past value
       ;;
       -prepRef|--prep_reference)
-      prepRef=$1
+      prepRef=$2
       shift # past argument
       shift # past value
       ;;
@@ -161,27 +142,27 @@ check_exit_status () {
       shift # past argument
       ;;
       -domap|--do_map)
-      do_map=$1
+      do_map=$2
       shift # past argument
       shift # past value
       ;;
       -dps|--do_poolsnp)
-      do_poolsnp=$1
+      do_poolsnp=$2
       shift # past argument
       shift # past value
       ;;
       -ds|--do_snape)
-      do_snape=$1
+      do_snape=$2
       shift # past argument
       shift # past value
       ;;
       -dopileup|--do_pileup)
-      do_pileup=$1
+      do_pileup=$2
       shift # past argument
       shift # past value
       ;;
       -docleanup|--do_cleanup)
-      do_cleanup=$1
+      do_cleanup=$2
       shift # past argument
       shift # past value
       ;;
@@ -197,6 +178,18 @@ check_exit_status () {
 #######################
 ### What am I doing ###
 #######################
+  if [ $do_single_end -eq "0"  ]; then
+    read1=$1; shift
+    read2=$1; shift
+    sample=$1; shift
+    output=$1; shift
+  fi
+
+  if [ $do_single_end -eq "1" ]; then
+    read1=$1; shift
+    sample=$1; shift
+    output=$1; shift
+  fi
 
   echo -e "This is DEST v.3.0.0 \n Parameters as interpreted + those assumed by default --> \n"
   echo -e \
@@ -226,89 +219,78 @@ check_exit_status () {
   "reference genome =" $ref "\n" \
   "focal chromosome file=" $focalFile "\n" \
 
-### error handling
-    ### prep ref errors
-      if [ "$prepRef" -eq "1" ] && [ "$do_poolsnp" -eq "1" ]; then
-        echo "Cannot prep ref and run mapping at once"
-        exit 1
-      fi
+  if [ ! -f "$read1" ] && [ $do_single_end -eq "0"  ]; then
+    echo "ERROR: for paired end run"
+    echo "ERROR DETAILS: $read1 does not exist"
+    exit 1
+  fi
 
-      if [ "$prepRef" -eq "1" ] && [ "$do_snape" -eq "1" ]; then
-        echo "Cannot prep ref and run mapping at once"
-        exit 1
-      fi
+  if [ ! -f "$read2" ] && [ $do_single_end -eq "0"  ]; then
+    echo "ERROR: for paired end run"
+    echo "ERROR DETAILS: $read2 does not exist"
+    exit 1
+  fi
 
-      if [ "$prepRef" -eq "1" ] && [ "$do_map" -eq "1" ]; then
-        echo "Cannot prep ref and run mapping at once"
-        exit 1
-      fi
-
-      if [ "$prepRef" -eq "1" ] && [ "$do_pileup" -eq "1" ]; then
-        echo "Cannot prep ref and run mapping at once"
-        exit 1
-      fi
-
-    ### checking that fastq files exist
-      if [ ! -f "$read1" ] && [ "$do_single_end" -eq "0"  ]; then
-        echo "ERROR: for paired end run"
-        echo "ERROR DETAILS: $read1 does not exist"
-        exit 1
-      fi
-
-      if [ ! -f "$read2" ] && [ "$do_single_end" -eq "0"  ]; then
-        echo "ERROR: for paired end run"
-        echo "ERROR DETAILS: $read2 does not exist"
-        exit 1
-      fi
-
-      if [ ! -f "$read1" ] && [ "$do_single_end" -eq "1"  ]; then
-        echo "ERROR: for single end run"
-        echo "ERROR DETAILS: $read1 does not exist"
-        exit 1
-      fi
+  if [ ! -f "$read1" ] && [ $do_single_end -eq "1"  ]; then
+    echo "ERROR: for single end run"
+    echo "ERROR DETAILS: $read1 does not exist"
+    exit 1
+  fi
 
 
 ################################
 ### prepare reference genome ###
 ################################
-  if [ $prepRef -eq "1" ]; then
-    if [ ! -f ${ref}.amb ]; then bwa index ${ref}; fi
-    if [ ! -f ${ref}.fai ]; then samtools faidx ${ref}; fi
-    if [ ! -f ${ref}.dict ]; then
-      refDict=$( echo ${ref} | sed 's/fa/dict/g' )
-      java -jar $PICARD CreateSequenceDictionary R=${ref} O=${refDict}
+  ### error handling
+    if [ $prepRef -eq "1" ] && [ $do_poolsnp -eq "1" ]; then
+      echo "Cannot prep ref and run mapping at once"
+      exit 1
     fi
 
-    makePickles () {
-      # ref=/scratch/aob2x/tmpRef/holo_dmel_6.12.fa; prefix=mel; chrs=2L
-      prefix=$( echo $1 | cut -f1 -d',')
-      echo $prefix
-      chrs=$( echo $1 | cut -f2 -d',')
-      echo $chrs
+    if [ $prepRef -eq "1" ] && [ $do_snape -eq "1" ]; then
+      echo "Cannot prep ref and run mapping at once"
+      exit 1
+    fi
 
-      picklesDir=$( echo $ref | awk -F'/' '{ for(i=1; i<NF; i++) printf $i"/"; printf "pickles"}' )
-      if [ ! -d "${picklesDir}" ]; then mkdir ${picklesDir}; fi
-      refStem=$( echo $ref | awk -F'/' '{print $NF}' )
-      refOut=${picklesDir}/${prefix}_${chrs}.$refStem
+  #### prep the reference genome
+    if [ $prepRef -eq "1" ]; then
+      if [ ! -f ${ref}.amb ]; then bwa index ${ref}; fi
+      if [ ! -f ${ref}.fai ]; then samtools faidx ${ref}; fi
+      if [ ! -f ${ref}.dict ]; then
+        refDict=$( echo ${ref} | sed 's/fa/dict/g' )
+        java -jar $PICARD CreateSequenceDictionary R=${ref} O=${refDict}
+      fi
 
-      echo "extracing chromosome: "$prefix $chrs
-      samtools faidx ${ref} ${chrs} > ${picklesDir}/${prefix}_${chrs}.$refStem
+      makePickles () {
+        # ref=/scratch/aob2x/tmpRef/holo_dmel_6.12.fa; prefix=mel; chrs=2L
+        prefix=$( echo $1 | cut -f1 -d',')
+        echo $prefix
+        chrs=$( echo $1 | cut -f2 -d',')
+        echo $chrs
 
-      echo "indexing chromosome: "$prefix $chrs
-      samtools faidx ${picklesDir}/${prefix}_${chrs}.$refStem
+        picklesDir=$( echo $ref | awk -F'/' '{ for(i=1; i<NF; i++) printf $i"/"; printf "pickles"}' )
+        if [ ! -d "${picklesDir}" ]; then mkdir ${picklesDir}; fi
+        refStem=$( echo $ref | awk -F'/' '{print $NF}' )
+        refOut=${picklesDir}/${prefix}_${chrs}.$refStem
 
-      echo "pickling chromosome: "$prefix $chrs
-      python3 /opt/DESTv3/mappingPipeline/scripts/PickleRef.py \
-      --ref ${picklesDir}/${prefix}_${chrs}.$refStem \
-      --output ${picklesDir}/${prefix}_${chrs}.$refStem
+        echo "extracing chromosome: "$prefix $chrs
+        samtools faidx ${ref} ${chrs} > ${picklesDir}/${prefix}_${chrs}.$refStem
 
-    }
-    export -f makePickles
-    export ref
+        echo "indexing chromosome: "$prefix $chrs
+        samtools faidx ${picklesDir}/${prefix}_${chrs}.$refStem
 
-    parallel -j ${threads} makePickles ::: $( cat $focalFile ) ### | awk -F'[, ]' '{for (i=2;i<=NF;i++) {if($i!="") print $1","$i}}' )
-    exit
-  fi
+        echo "pickling chromosome: "$prefix $chrs
+        python3 /opt/DESTv3/mappingPipeline/scripts/PickleRef.py \
+        --ref ${picklesDir}/${prefix}_${chrs}.$refStem \
+        --output ${picklesDir}/${prefix}_${chrs}.$refStem
+
+      }
+      export -f makePickles
+      export ref
+
+      parallel -j ${threads} makePickles ::: $( cat $focalFile ) ### | awk -F'[, ]' '{for (i=2;i<=NF;i++) {if($i!="") print $1","$i}}' )
+      exit
+    fi
 
 ######################################
 ### do_map? Process and map reads? ###

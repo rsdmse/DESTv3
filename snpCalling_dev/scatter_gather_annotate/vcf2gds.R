275a2ad3521cbac42aa47dbc5505cb3683e5305b
Alan Bergland <aob2x@virginia.edu>
2026-03-04 11:06:53 -0500

    udpate

--- snpCalling_dev/scatter_gather_annotate/vcf2gds.R
+++ snpCalling_dev/scatter_gather_annotate/vcf2gds.R
@@ -1,16 +1,17 @@
-# ijob -A biol4559-aob2x -c10 -p largemem --mem=40G
+# ijob -A berglandlab -c40 -p standard --mem=100G
 # module load gcc/7.1.0 openmpi/3.1.4 R/3.6.3; R
 
 library(SeqArray)
 
+#vcf.fn="/project/berglandlab/Dmel_Single_Individuals/Phased_Whatshap_only/CM_AB2016_PE2018_Dmel_dm6.whatshapp.noSR.vcf.gz"
 
 args = commandArgs(trailingOnly=TRUE)
 vcf.fn=args[[1]]
 nCores=as.numeric(args[[2]])
 gds.fn=gsub(".vcf", ".gds", vcf.fn)
-gds.fun=gsub(".gz", "", gds.fn)
+gds.fn=gsub(".gz", "", gds.fn)
+
 #vcf.fn=paste(vcf.fn, ".gz", sep="")
-#vcf.fn="/scratch/aob2x/14Nov2025_sim_dest3/dest.sim.all.SNAPE.001.50.14Nov2025_sim.norep.ann.vcf"
 seqParallelSetup(cluster=nCores, verbose=TRUE)
 
 seqVCF2GDS(vcf.fn, gds.fn, storage.option="ZIP_RA", parallel=nCores, verbose=T, optimize=T)
